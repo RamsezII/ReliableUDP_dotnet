b8965284ce78e76d0c15696a48791125cd2838a4
ram16 <thomass.montaine@gmail.com>
2024-01-25 15:47:53 +0100

    okè c parti

---
+++ ReliableUDP_dotnet/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("> ");
+        string? input = Console.ReadLine();
+        Console.WriteLine($"T'as dis \"{input}\", sur ce JME CASSE");
+    }
+}
